dc92b44795f7c8befe227bbdc9d6319121454033
Sivakumar, Mithusan <mithusan.sivakumar@zuehlke.com>
2022-04-26 17:25:23 +0200

    initial setup

---
+++ backend/src/test/java/com/zuehlke/hatch/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuehlke.hatch.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
